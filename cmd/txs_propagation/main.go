59dea397ef5b8e8634bb800bfa31ff031210e589
PeepoFrog <organizm999@gmail.com>
2023-05-18 17:03:41 +0300

    added block listener and transaction for range of users

--- cmd/txs_propagation/main.go
+++ cmd/txs_propagation/main.go
@@ -15,15 +15,25 @@ func main() {
 		panic(err)
 	}
 
-	type users struct {
+	type user struct {
 		key string
 	}
+	c := make(chan int)
 	waitGroup := &sync.WaitGroup{}
-	var arr []users = []users{{key: "kira1ap96a0dpx0mdjv9mxr7pnryx67hrry5melcs8n"}, {key: "kira1rh9wufxa9sxre53zapsvajsvyqvfvrhmckndta"}}
+	waitGroup.Add(1)
+
+	go docker.BlockListener(client, "validator", "1578", waitGroup, c)
+
+	//блокуєм виконання за допомогою читання з канала, запис відбудеться лише тоді коли блок досягне певної висоти
+	<-c
+
+	var arr []user = []user{{key: "kira1ap96a0dpx0mdjv9mxr7pnryx67hrry5melcs8n"}, {key: "kira1rh9wufxa9sxre53zapsvajsvyqvfvrhmckndta"}}
 	for _, b := range arr {
 		waitGroup.Add(1)
-		go docker.RunTransaction(client, "validator", b.key, b.key, "1", "ukex", waitGroup, 5, 10)
+		go docker.RunTransaction(client, "validator", b.key, b.key, "1", "ukex", waitGroup, 5, 1)
 
 	}
+
 	waitGroup.Wait()
+
 }
--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"regexp"
 	"sync"
 	"time"
 
@@ -11,6 +12,39 @@ import (
 	"github.com/docker/docker/client"
 )
 
+func BlockListener(dockerClient *client.Client, containerName, blockToListen string, wg *sync.WaitGroup, c chan int) {
+	var currentBlockHeight string
+	for currentBlockHeight != blockToListen {
+		time.Sleep(time.Second * 1)
+		currentBlockHeight = GetBlockHeight(dockerClient, containerName)
+		fmt.Println("current", currentBlockHeight)
+
+	}
+	c <- 1
+	fmt.Println("curent:", currentBlockHeight, "goal:", blockToListen)
+	defer wg.Done()
+
+}
+func GetBlockHeight(dockerClient *client.Client, containerName string) string {
+	command := "sekaid status"
+	out, err := ExecCommandInContainer(containerName, []string{`bash`, `-c`, command}, dockerClient)
+	if err != nil {
+		panic(err)
+	}
+	str := string(out)
+	re := regexp.MustCompile(`"latest_block_height":"(\d+)"`) // Regular expression pattern
+
+	match := re.FindStringSubmatch(str) // Find the match in the string
+	var value string
+	if len(match) == 2 {
+		value = match[1] // Extract the value from the match
+		fmt.Println("latest_block_height:", value)
+	} else {
+		fmt.Println("latest_block_height not found")
+	}
+	fmt.Println(string(out))
+	return value
+}
 func RunTransaction(dockerClient *client.Client, containerName, source, destination, amount, demon string, wg *sync.WaitGroup, txAmount, sleepTimeBetweenTx int) {
 	//    sekaid tx bank send $SOURCE $DESTINATION "${AMOUNT}${DENOM}" --keyring-backend=test --chain-id=$NETWORK_NAME --fees "${FEE_AMOUNT}${FEE_DENOM}" --output=json --yes --home=$SEKAID_HOME | txAwait 180
 	for i := 0; i < txAmount; i++ {
@@ -23,7 +57,7 @@ func RunTransaction(dockerClient *client.Client, containerName, source, destinat
 		fmt.Println(string(out))
 		time.Sleep(time.Second * time.Duration(sleepTimeBetweenTx))
 	}
-	wg.Done()
+	defer wg.Done()
 	//sekaid tx bank send kira1rh9wufxa9sxre53zapsvajsvyqvfvrhmckndta kira1cv89u97thwm837uzlcxh8yc0hele0jxwsklk93 100ukex --keyring-backend=test --chain-id=$NETWORK_NAME --fees 100ukex --output=json --yes --home=$SEKAID_HOME | txAwait 180
 
 }
