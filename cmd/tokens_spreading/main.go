123c5f50e7045a45ced669691e82c69ea6706810
DmitriyBrch <organizm999@gmail.com>
2023-05-27 12:18:23 +0300

    added seperate run main func for tokens spreading

---
+++ cmd/tokens_spreading/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/docker/docker/client"
+	"github.com/mrlutik/autoflowhub/pkg/keygen/usecase"
+
+	"github.com/mrlutik/autoflowhub/internal/docker"
+)
+
+func main() {
+	disruptSum := (7000000 / 4) * 100
+	KeysPath := "keydir"
+	client, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	reader := usecase.NewKeysReader(KeysPath)
+	list, err := reader.GetAllAddresses()
+	if err != nil {
+		panic(err)
+	}
+	var arr []*docker.User = make([]*docker.User, 2500)
+	for i := range list {
+		arr[i] = &docker.User{Key: list[i], Balance: 0}
+	}
+	// var arr []*docker.User = make([]*docker.User, 2500)
+	// for i := 0; i < 2500; i++ {
+	// 	arr[i] = &docker.User{Key: "pepelaugh", Balance: 0}
+	// }
+	waitGroup := &sync.WaitGroup{}
+	fmt.Println(arr[0])
+	docker.DisruptTokensBetweenAllAccounts(client, waitGroup, disruptSum, arr)
+	waitGroup.Wait()
+	for u := range arr {
+		fmt.Println(arr[u])
+	}
+
+}
