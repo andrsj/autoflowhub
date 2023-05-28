bbdd16f056e5585cc4808137b536df425c0780c9
PeepoFrog <organizm999@gmail.com>
2023-05-28 16:35:33 +0300

    added os args calls

--- cmd/tokens_spreading/main.go
+++ cmd/tokens_spreading/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"sync"
 
 	"github.com/docker/docker/client"
@@ -11,8 +13,21 @@ import (
 )
 
 func main() {
-	disruptSum := (7000000 / 4) * 100
-	KeysPath := "./keyTest2"
+	if len(os.Args) < 3 {
+		fmt.Printf("ERROR: \nUSSAGE: main <arg1> <arg2> <arg3>\narg1=total tx amount, if arg1=0 then default value=7000000/4 \narg2=folder with keys\n")
+		os.Exit(1)
+	}
+	// disruptSum := (7000000 / 4) * 100
+	disruptSum, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	if disruptSum == 0 {
+		disruptSum = 7000000
+	}
+	disruptSum = disruptSum * 100
+	KeysPath := os.Args[2]
+
 	client, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
